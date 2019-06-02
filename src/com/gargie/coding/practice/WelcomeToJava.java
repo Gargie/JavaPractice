9ee521bf0437a1bb58554a65a739cd98079c9fed
b0081397 <gargie.bhatia@airtel.com>
2019-06-02 20:58:10 +0530

    First

---
+++ src/com/gargie/coding/practice/WelcomeToJava.java
@@ -0,0 +1,11 @@
+package com.gargie.coding.practice;
+
+public class WelcomeToJava {
+
+	public static void main(String[] args) {
+
+		System.out.println("Hello, World.");
+		System.out.println("Hello, Java.");
+
+	}
+}
